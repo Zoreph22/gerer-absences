3a03f155bc0c90f188db4836daa83bd319bbb62b
Zoreph <61715886+Zoreph22@users.noreply.github.com>
2023-02-01 19:26:58 +0100

    Add SQL Schema Script
    Add SQL Schema Script

---
+++ db/init_sqlite.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS Apprenant;
+DROP TABLE IF EXISTS Groupe;
+DROP TABLE IF EXISTS Absences;
+
+CREATE TABLE Apprenant (
+  code_apprenant      INTEGER PRIMARY KEY AUTOINCREMENT,
+  code_groupe         INTEGER,
+  nom_apprenant       VARCHAR(200) NOT NULL,
+  prenom_apprenant    VARCHAR(200) NOT NULL,
+  CONSTRAINT FK_GroupeApprenant_Groupe FOREIGN KEY (code_groupe) REFERENCES Groupe(code_groupe)
+);
+
+CREATE TABLE Groupe (
+  code_groupe         INTEGER PRIMARY KEY AUTOINCREMENT,
+  nom_groupe          VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE Absences (
+  code_absence        INTEGER PRIMARY KEY AUTOINCREMENT,
+  code_apprenant      INTEGER,
+  nb_heures_absence   INTEGER NOT NULL,
+  date_absence        DATE NOT NULL,
+  CONSTRAINT FK_ApprenantAbsences_Apprenant FOREIGN KEY (code_apprenant) REFERENCES Apprenant(code_apprenant) ON DELETE CASCADE
+);
